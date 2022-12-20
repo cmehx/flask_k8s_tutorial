5ca77244e22d6b2a08040070e6b5e87f9c4b0c65
Mehdi <94063730+cmehx@users.noreply.github.com>
2022-12-20 15:04:54 +0100

    Update port to string

--- front/app.py
+++ front/app.py
@@ -6,7 +6,7 @@ from flask import Flask
 from redis import Redis
 
 redis = Redis(host=os.getenv('REDIS_HOST', 'localhost'),
-              port=os.getenv('REDIS_PORT', 6379))
+              port=os.getenv('REDIS_PORT', '6379'))
 app = Flask(__name__)
 
 @app.route('/')
